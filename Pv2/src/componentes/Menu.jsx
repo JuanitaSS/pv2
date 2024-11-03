d93595fda482a1837b765d4366ea8d2a82b15c87
Foster-11 <rt.susana@hotmail.com>
2024-11-02 23:33:49 -0500

    Funcion que recibe informacion del user,se lleva a los componentes de menu

--- Pv2/src/componentes/Menu.jsx
+++ Pv2/src/componentes/Menu.jsx
@@ -1,28 +1,36 @@
-import { Link } from 'react-router-dom';
-import estilos from '../estilos/Menu.module.css'; 
+import { Link, useLocation } from 'react-router-dom'; 
+import estilos from '../estilos/Menu.module.css';
+
 
 function Menu() {
+
+  const location = useLocation();
+  const user = location.state?.user; 
+  if (!user) {
+    return <p>Error: No se encontró información del usuario.</p>; 
+  }
+  
   return (
     <div className={estilos.menu}>
       <h2 className={estilos.logo}>Estebanquito</h2>
       <nav className={estilos.nav}>
-        <Link to="/plataforma" className={estilos.menuItem}>
+        <Link to="/plataforma" state ={user} className={estilos.menuItem}>
           <i className="material-icons">home</i> 
         </Link>
-        <Link to="/saldos" className={estilos.menuItem}>
+        <Link to="/saldos" state ={user} className={estilos.menuItem}>
           <i className="material-icons">date_range</i> 
         </Link>
-        <Link to="/transferencias" className={estilos.menuItem}>
+        <Link to="/transferencias" state ={user} className={estilos.menuItem}>
           <i className="material-icons">near_me	
           </i> 
         </Link>
-        <Link to="/prestamos" className={estilos.menuItem}>
+        <Link to="/prestamos" state ={user} className={estilos.menuItem}>
           <i className="material-icons">business_center</i> 
         </Link>
-        <Link to="/pago" className={estilos.menuItem}>
+        <Link to="/pago" state ={user} className={estilos.menuItem}>
           <i className="material-icons">attach_money</i> 
         </Link>
-        <Link to="/yo" className={estilos.menuItem}>
+        <Link to="/yo" state ={user} className={estilos.menuItem}>
           <i className="material-icons">account_circle</i> 
         </Link>
       </nav>
